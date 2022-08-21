36cc071774cfe95e02658706867a45283962028b
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-08-21 14:58:32 +0200

    Model biletu

--- ASP.NET_Core/Event.cs
+++ ASP.NET_Core/Event.cs
@@ -33,9 +33,11 @@ namespace Evento.Core.Domain
 
         public void AddTickets(int amount, decimal price)
         {
+            var seating = _tickets.Count + 1;
             for(var i=0; i<amount; i++)
             {
-                _tickets.Add(new Ticket());
+                _tickets.Add(new Ticket(this, seating, price));
+                seating++;
             }
         }
     }
