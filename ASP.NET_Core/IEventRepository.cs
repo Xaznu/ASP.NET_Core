78abb3d80bad624983b6009af3b4e1e754d15c5a
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-08-21 15:14:53 +0200

    Tworzenie i ustawienie - Repozytorium

---
+++ ASP.NET_Core/IEventRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Evento.Core.Domain;
+
+namespace Evento.Core.Repositories
+{
+
+public interface IEventRepository
+{
+		Task<Event> GetAsync(Guid id);
+		Task<Event> GetAsync(string name);
+		Task<Enumerable<Event>> BrowseSync(string name = "");
+		Task GetAsync(Event @event);
+		Task UpdateAsync((Event @event);
+		Task DeleteAsync(Event @event);
+
+
+
+	}
+}
---
+++ ASP.NET_Core/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Evento.Core.Domain ;
+
+namespace Evento.Core.Repositories
+{
+
+	public interface IUserRepository
+	{
+		Task<User> GetAsync(Guid id);
+		Task<User> GetAsync(string email);
+		Task GetAsync(User user);
+		Task UpdateAsync(User user);
+		Task DeleteAsync(User user);
+	}
+}
