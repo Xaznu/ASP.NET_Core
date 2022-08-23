47aaf79991fbd1066d770c75dc656547f28f06f0
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-08-23 12:14:24 +0200

    Szczegóły wydarzenia

--- ASP.NET_Core/DateTimeExtensions.cs
+++ ASP.NET_Core/DateTimeExtensions.cs
@@ -1,8 +1,15 @@
 ﻿using System;
 
-public class Class1
+namespace Evento.Infrastructure.Extensions
 {
-	public Class1()
-	{
-	}
+    public static class DateTimeExtensions
+    {
+        public static long ToTimestamp(this DateTime dateTime)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            var time = dateTime.Subtract(new TimeSpan(epoch.Ticks));
+
+            return time.Ticks / 10000;
+        }
+    }
 }
